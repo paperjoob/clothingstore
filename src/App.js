3c7d6444b01b1c71990466414532afdaebdfc1b6
See Yang <seeyang24@gmail.com>
2020-11-25 10:19:31 -0600

    created header page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepages/HomePage';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      {/* place header outside of the Switch and Routes to make it always visible */}
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
